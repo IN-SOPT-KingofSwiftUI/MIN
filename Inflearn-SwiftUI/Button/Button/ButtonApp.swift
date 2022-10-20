dd836c912529256370c330584764ea8d8bbc6c8a
Mini <minnnidev@gmail.com>
2022-10-20 19:05:42 +0900

    style: Button 만들어 보기 (#1)

---
+++ Inflearn-SwiftUI/Button/Button/ButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonApp.swift
+//  Button
+//
+//  Created by 김민 on 2022/10/20.
+//
+
+import SwiftUI
+
+@main
+struct ButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
