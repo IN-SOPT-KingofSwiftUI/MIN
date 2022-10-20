4a370928ac976abd17852dc2bf8b79a536137e50
Mini <minnnidev@gmail.com>
2022-10-20 19:23:38 +0900

    style: Image 추가하기 (#1)

---
+++ Inflearn-SwiftUI/Image/Image/ImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageApp.swift
+//  Image
+//
+//  Created by 김민 on 2022/10/20.
+//
+
+import SwiftUI
+
+@main
+struct ImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
