cbcfa96c9aaef971daed4241e9712dd63d879fd9
Mini <minnnidev@gmail.com>
2022-10-20 19:08:54 +0900

    test: test commit

---
+++ Inflearn-SwiftUI/Text/Text/TextApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextApp.swift
+//  Text
+//
+//  Created by 김민 on 2022/10/20.
+//
+
+import SwiftUI
+
+@main
+struct TextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
