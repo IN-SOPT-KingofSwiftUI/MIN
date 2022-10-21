236197a0a46cd58d534f1ad290da5985085e8adb
Mini <minnnidev@gmail.com>
2022-10-22 01:09:36 +0900

    style: Stack 사용해 보기 (#1)

---
+++ Inflearn-SwiftUI/Stack/Stack/StackApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackApp.swift
+//  Stack
+//
+//  Created by 김민 on 2022/10/22.
+//
+
+import SwiftUI
+
+@main
+struct StackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
