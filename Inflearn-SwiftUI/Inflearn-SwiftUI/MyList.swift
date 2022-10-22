d788a1b6c06da71a47fb308bb77b1bf08f3f1aaa
Mini <minnnidev@gmail.com>
2022-10-22 14:06:26 +0900

    style: List 사용해 보기 (#2)

---
+++ Inflearn-SwiftUI/Inflearn-SwiftUI/MyList.swift
@@ -0,0 +1,57 @@
+//
+//  MyList.swift
+//  Inflearn-SwiftUI
+//
+//  Created by 김민 on 2022/10/22.
+//
+
+import SwiftUI
+
+struct Person: Identifiable {
+    var id = UUID()
+    let name: String
+    let imageName: String
+}
+
+struct MyList: View {
+    var body: some View {
+        let people: [Person] = [
+            Person(name: "Kim", imageName: "heart"),
+            Person(name: "Lee", imageName: "heart.fill"),
+            Person(name: "Park", imageName: "bolt")
+        ]
+        
+        List(people) { person in
+            HStack {
+                Image(systemName: person.imageName)
+                Text(person.name)
+            }
+        }
+        
+        /*
+        List {
+            HStack {
+                Image(systemName: "heart")
+                Text("first")
+            }
+            
+            HStack {
+                Image(systemName: "heart.fill")
+                Text("second")
+            }
+            
+            HStack {
+                Image(systemName: "bolt")
+                Text("third")
+            }
+        }
+         */
+    }
+   
+}
+
+struct MyList_Previews: PreviewProvider {
+    static var previews: some View {
+        MyList()
+    }
+}
