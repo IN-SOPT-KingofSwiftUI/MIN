c123a513fe80ba7ce371cf976f2ac91171c4cdd0
Mini <minnnidev@gmail.com>
2022-10-22 13:54:34 +0900

    style: color 이용하기 (#1)

---
+++ Inflearn-SwiftUI/Color/Color/ColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorApp.swift
+//  Color
+//
+//  Created by 김민 on 2022/10/22.
+//
+
+import SwiftUI
+
+@main
+struct ColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
