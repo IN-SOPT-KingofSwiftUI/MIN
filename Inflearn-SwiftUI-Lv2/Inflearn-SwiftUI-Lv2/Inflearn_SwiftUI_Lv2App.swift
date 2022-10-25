fdc63a702d7a25db8530f3278287806c7da7702d
Mini <minnnidev@gmail.com>
2022-10-25 17:34:15 +0900

    init: 프로젝트 생성 (#4)

---
+++ Inflearn-SwiftUI-Lv2/Inflearn-SwiftUI-Lv2/Inflearn_SwiftUI_Lv2App.swift
@@ -0,0 +1,17 @@
+//
+//  Inflearn_SwiftUI_Lv2App.swift
+//  Inflearn-SwiftUI-Lv2
+//
+//  Created by 김민 on 2022/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Inflearn_SwiftUI_Lv2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
