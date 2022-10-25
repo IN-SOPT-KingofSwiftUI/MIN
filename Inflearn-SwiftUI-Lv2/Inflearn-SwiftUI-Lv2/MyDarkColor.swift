0e28d684a94bc79577fb06945593d788aff6de85
Mini <minnnidev@gmail.com>
2022-10-25 17:41:08 +0900

    chore: color asset 추가 (#4)

---
+++ Inflearn-SwiftUI-Lv2/Inflearn-SwiftUI-Lv2/MyDarkColor.swift
@@ -0,0 +1,21 @@
+//
+//  MyDarkColor.swift
+//  Inflearn-SwiftUI-Lv2
+//
+//  Created by 김민 on 2022/10/25.
+//
+
+import SwiftUI
+
+struct MyDarkColor: View {
+    var body: some View {
+        Text("Mini!")
+            .background(Color("MinsColor"))
+    }
+}
+
+struct MyDarkColor_Previews: PreviewProvider {
+    static var previews: some View {
+        MyDarkColor()
+    }
+}
