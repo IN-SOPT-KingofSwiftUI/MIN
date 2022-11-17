cf16674144a4405051a3ae010c9584c7f515a27d
Mini <minnnidev@gmail.com>
2022-11-17 12:08:14 +0900

    [Feat] 매인 화면 뷰 구현 #5

---
+++ Mail-App-Clone/Mail-App-Clone/Mail_App_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mail_App_CloneApp.swift
+//  Mail-App-Clone
+//
+//  Created by 김민 on 2022/11/17.
+//
+
+import SwiftUI
+
+@main
+struct Mail_App_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
